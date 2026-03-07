9a8ccd1c03a28f61b7159daaa1806f149ee5330b
dudpatil07 <dudpatil07@gmail.com>
2026-03-07 15:18:22 +0530

    Fix: completed conditionals and exception handling exercise

--- shubh_solutions/test_playground/basics/conditionals.py
+++ shubh_solutions/test_playground/basics/conditionals.py
@@ -1,26 +1,38 @@
 # correct if else ladder to check if person is underage, normal citizen or senior citizen
 # [0,18) -> underage, [18,60) normal age, [60,inf) senior citizen
 # bonus, can you reduce ladder to a one liner?
-age = input("Enter age") # ahh yes age is str , definitely
+age = int(input("Enter age")) # ahh yes age is str , definitely
 
-if age <= 0:
-    print("Lil bro")
-elif age > 100:
-    print("Pay up taxes, person")
+if age <= 18:
+    print("underage")
+elif age >60:
+    print("normal age")
 else:
-    print("U still good, unc?")
+    print("senior citizen")
 
 
 # complete the match
 
-day = input("Enter the day number") # dont forget to typecast to int
+day = int(input("Enter the day number")) # dont forget to typecast to int
 
-print("Today is: ") # how can you avoid printing newline here?
+print("Today is: ",end="") # how can you avoid printing newline here?
 
 match day:
     case 1:
         print("Monday")
     # fill in the rest
+    case 2:
+        print("tuesday")
+    case 3:
+        print("wednesday")
+    case 4:
+        print("thrusday")   
+    case 5:
+        print("friday")
+    case 6:
+        print("saturday") 
+    case 7:
+        print("sunday")            
     case _:
         print("Funday !") 
 
@@ -28,7 +40,7 @@ match day:
 
 try:
     print(1/0)
-except IndentationError: # ahh fix the syntax, also when u don't know the error what will u use?
+except Exception: # ahh fix the syntax, also when u don't know the error what will u use?
     print("what u tryna do bro")
 finally:
     print("So u done?")
